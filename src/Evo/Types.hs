c403b2847842b3acf1dcdd9e01e264bfc6e314ee
Ting-Yen Lai <banacorn@gmail.com>
2014-08-11 20:27:42 +0800

    cleanup

--- src/Evo/Types.hs
+++ src/Evo/Types.hs
@@ -20,68 +20,68 @@ instance (MonadBaseControl IO) EvoM where
     liftBaseWith f = EvoM (liftBaseWith (\run -> f (liftM StMEvo . run . runEvoM)))
     restoreM = EvoM . restoreM . unStMCEvo
 
-data Parameter = Parameter
-    {   _populationSize :: Int 
-    ,   _learningRate :: Double
-    ,   _discountFactor :: Double
-    ,   _errorBound :: Double
-    ,   _falloffRate :: Double
-    ,   _initialPrediction :: Double
-    ,   _initialError :: Double
-    ,   _initialFitness :: Double
+--data Parameter = Parameter
+--    {   _populationSize :: Int 
+--    ,   _learningRate :: Double
+--    ,   _discountFactor :: Double
+--    ,   _errorBound :: Double
+--    ,   _falloffRate :: Double
+--    ,   _initialPrediction :: Double
+--    ,   _initialError :: Double
+--    ,   _initialFitness :: Double
 
-    ,   _chromosomeLength :: Int
-    ,   _crossoverProb :: Double
-    ,   _mutationProb :: Double
-    }
+--    ,   _chromosomeLength :: Int
+--    ,   _crossoverProb :: Double
+--    ,   _mutationProb :: Double
+--    }
 
-type RuleSet c a = [Rule c a]
-type MatchSet c a = RuleSet c a
-type ActionSet c a = RuleSet c a
-type Classifier c a = RuleSet c a -> [c] -> EvoM a
-type Model c a = [c] -> a
+--type RuleSet c a = [Rule c a]
+--type MatchSet c a = RuleSet c a
+--type ActionSet c a = RuleSet c a
+--type Classifier c a = RuleSet c a -> [c] -> EvoM a
+--type Model c a = [c] -> a
 
-data Rule c a = Rule
-    {   _condition :: [c]
-    ,   _action :: a
-    ,   _prediction :: Double
-    ,   _error :: Double
-    ,   _fitness :: Double
-    }
+--data Rule c a = Rule
+--    {   _condition :: [c]
+--    ,   _action :: a
+--    ,   _prediction :: Double
+--    ,   _error :: Double
+--    ,   _fitness :: Double
+--    }
 
-instance (Show c, Show a) => Show (Rule c a) where
-    show (Rule cond action prediction err fitness) = "< " ++ concat (map show cond) 
-        ++ " : " ++ show action 
-        ++ " = " ++ show prediction 
-        ++ " " ++ show err
-        ++ " " ++ show fitness
-        ++ " >"
+--instance (Show c, Show a) => Show (Rule c a) where
+--    show (Rule cond action prediction err fitness) = "< " ++ concat (map show cond) 
+--        ++ " : " ++ show action 
+--        ++ " = " ++ show prediction 
+--        ++ " " ++ show err
+--        ++ " " ++ show fitness
+--        ++ " >"
 
-class (Eq c, Enum c, Show c, Variate c, Bounded c) => Condition c where
-    dontCare :: c
+--class (Eq c, Enum c, Show c, Variate c, Bounded c) => Condition c where
+--    dontCare :: c
 
-class (Eq a, Enum a, Show a, Variate a, Bounded a) => Action a where
+--class (Eq a, Enum a, Show a, Variate a, Bounded a) => Action a where
 
-data Bit = DontCare | On | Off deriving (Enum, Bounded)
+--data Bit = DontCare | On | Off deriving (Enum, Bounded)
 
-instance Eq Bit where
-    On == Off = False
-    On == _ = True
-    Off == On = False
-    Off == _ = True
-    DontCare == _ = True
+--instance Eq Bit where
+--    On == Off = False
+--    On == _ = True
+--    Off == On = False
+--    Off == _ = True
+--    DontCare == _ = True
 
-instance Show Bit where
-    show On = "1"
-    show Off = "0"
-    show DontCare = "#"
+--instance Show Bit where
+--    show On = "1"
+--    show Off = "0"
+--    show DontCare = "#"
 
-instance Condition Bit where
-    dontCare = DontCare
-instance Action Bool
+--instance Condition Bit where
+--    dontCare = DontCare
+--instance Action Bool
 
-instance Variate Bit where
-    uniform gen = uniform gen >>= return . toEnum . flip mod 3
-    uniformR (a, b) gen = uniformR (fromEnum a, fromEnum b) gen >>= return . toEnum
+--instance Variate Bit where
+--    uniform gen = uniform gen >>= return . toEnum . flip mod 3
+--    uniformR (a, b) gen = uniformR (fromEnum a, fromEnum b) gen >>= return . toEnum
 
-type Vanilla = Rule Bit Bool
+--type Vanilla = Rule Bit Bool
